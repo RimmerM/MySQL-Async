7a8ebb2646bcc7236ffda9200bb6f7bacf10754e
Le-Chiffre <rimmer.motzheim@gmail.com>
2016-03-03 00:58:16 +0100

    Created DSL builder helper

---
+++ src/com/rimmer/mysql/dsl/Builder.kt
@@ -0,0 +1,23 @@
+package com.rimmer.mysql.dsl
+
+inline fun <E> iterateLast(i: Iterator<E>, element: (E) -> Unit, last: (E) -> Unit) {
+    if(i.hasNext()) {
+        while(true) {
+            val e = i.next()
+            if(i.hasNext()) {
+                element(e)
+            } else {
+                last(e)
+                break
+            }
+        }
+    }
+}
+
+inline fun <E> Collection<E>.iterateLast(element: (E) -> Unit, last: (E) -> Unit) = iterateLast(iterator(), element, last)
+
+inline fun <E> sepBy(i: Iterator<E>, string: StringBuilder, sep: String, f: (E) -> Unit) {
+    iterateLast(i, {f(it); string.append(sep)}, {f(it)})
+}
+
+inline fun <E> Collection<E>.sepBy(string: StringBuilder, sep: String, f: (E) -> Unit) = sepBy(iterator(), string, sep, f)
