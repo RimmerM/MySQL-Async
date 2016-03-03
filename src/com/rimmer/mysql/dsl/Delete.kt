71cf2c133f958a750a2de342740f22cf766c6b00
Le-Chiffre <rimmer.motzheim@gmail.com>
2016-03-03 10:08:42 +0100

    Added delete query

---
+++ src/com/rimmer/mysql/dsl/Delete.kt
@@ -0,0 +1,22 @@
+package com.rimmer.mysql.dsl
+
+import com.rimmer.mysql.protocol.Connection
+import com.rimmer.mysql.protocol.QueryResult
+
+class DeleteQuery(val table: Table, val predicate: Op<Boolean>? = null, val isIgnore: Boolean = false): Query {
+    override fun run(c: Connection, f: (QueryResult?, Throwable?) -> Unit) {
+        val builder = QueryBuilder()
+
+        builder.append("DELETE ")
+        if(isIgnore) { builder.append("IGNORE ") }
+        builder.append("FROM ")
+        table.format(builder)
+
+        if(predicate != null) {
+            builder.append(" WHERE ")
+            predicate.format(builder)
+        }
+
+        builder.run(c, f)
+    }
+}
