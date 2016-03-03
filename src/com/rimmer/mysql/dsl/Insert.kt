31fc7a72d1166c2a6bb2da8df1662e2bf0fe7a0a
Le-Chiffre <rimmer.motzheim@gmail.com>
2016-03-03 01:00:38 +0100

    Added DSL batch insert creator

--- src/com/rimmer/mysql/dsl/Insert.kt
+++ src/com/rimmer/mysql/dsl/Insert.kt
@@ -41,3 +41,65 @@ class Insert(val table: Table, val isIgnore: Boolean = false, val isReplace: Boo
         builder.run(c, f)
     }
 }
+
+class BatchInsert(val table: Table, val isIgnore: Boolean = false, val isReplace: Boolean = false) {
+    val values = ArrayList<ArrayList<Any?>>()
+    val columns = ArrayList<Column<*>>()
+
+    fun addBatch() {
+        if(values.size > 0 && values.last().size != columns.size) {
+            throw IllegalArgumentException("Every inserted batch must have the same number of columns.")
+        }
+
+        values.add(ArrayList())
+    }
+
+    operator fun <T> set(column: Column<T>, value: T) {
+        val batch = values.last()
+        if(values.size > 1) {
+            if(columns.size <= batch.size || columns[batch.size] !== column) {
+                throw IllegalArgumentException("Every inserted batch must contain the same columns.")
+            }
+        } else {
+            columns.add(column)
+        }
+
+        batch.add(value)
+    }
+
+    fun run(c: Connection, f: (QueryResult?, Throwable?) -> Unit) {
+        if(values.isEmpty()) {
+            f(QueryResult(0, 0, "", 0, null), null)
+            return
+        }
+
+        val builder = QueryBuilder()
+        builder.append("INSERT ")
+        if(isIgnore) { builder.append("IGNORE ") }
+
+        builder.append("INTO ")
+        table.format(builder)
+        builder.append(" (")
+        sepBy(columns.iterator(), builder.string, ", ") { it.format(builder) }
+        builder.append(") VALUES ")
+        sepBy(values.iterator(), builder.string, ", ") {
+            builder.append('(')
+            sepBy(it.iterator(), builder.string, ", ") {
+                builder.argument(it)
+            }
+            builder.append(')')
+        }
+
+        if(isReplace) {
+            builder.append("ON DUPLICATE KEY UPDATE ")
+            sepBy(columns.iterator(), builder.string, ", ") {
+                it.format(builder)
+                builder.append("=VALUES(")
+                it.format(builder)
+                builder.append(')')
+            }
+        }
+
+        builder.run(c, f)
+    }
+}
